7cba8282f198dc0b4fcadc3fccddb3f97c6fcbd8
Flavio C <ing.flavio.cordari@gmail.com>
2022-05-08 18:05:15 +0200

    fix

--- processors.py
+++ processors.py
@@ -20,7 +20,7 @@ class Processor(ABC):
         self.before_run()
 
         executable = self.ctx['processors'][self.__class__.__name__]['executable']
-        if len(self.ctx['processors'][self.__class__.__name__]['args']) != 0:
+        if self.ctx['processors'][self.__class__.__name__]['args']:
             args = [executable, *self.ctx['processors'][self.__class__.__name__]['args']]
         else:
             args = [executable]
