514dca917c6cba6550e0d5d7547f06cc5c6f0187
Flavio C <ing.flavio.cordari@gmail.com>
2022-10-19 12:21:10 +0200

    feat: powershell run script root folder auto recon

--- run.ps1
+++ run.ps1
@@ -1,10 +1,10 @@
 Invoke-Expression -Command C:\ProgramData\Miniconda3\shell\condabin\conda-hook.ps1
 
-conda activate e2es-orchestrator
+$PSScriptRoot = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
 
-$ContextFolder = 'C:\orchestrator\context'
-$SnakemakeFolder = 'C:\orchestrator\.snakemake'
-$CacheFolder = 'C:\orchestrator\__pycache__'
+$ContextFolder = $PSScriptRoot + '\context'
+$SnakemakeFolder = $PSScriptRoot + '\.snakemake'
+$CacheFolder = $PSScriptRoot + '\__pycache__'
 if (Test-Path $ContextFolder) {
     Remove-Item $ContextFolder -Recurse -Force
 }
@@ -15,5 +15,5 @@ if (Test-Path $CacheFolder) {
     Remove-Item $CacheFolder -Recurse -Force
 }
 
-
-cd C:\orchestrator; snakemake --cores 1 TARGET
+conda activate e2es-orchestrator
+cd $PSScriptRoot; snakemake --cores 1 TARGET
