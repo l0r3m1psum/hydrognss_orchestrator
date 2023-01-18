93fe9b8cd383c4f0c08edf847873d99023a7f2bd
l0r3m1psum <bellanidiego@gmail.com>
2023-01-18 11:58:34 +0100

    Added line for possible future use.

--- core/processors.py
+++ core/processors.py
@@ -177,6 +177,7 @@ class L1_A(Processor):
         self.ctx['referenceFromL1A'] = pam_data_path_src
         self.log.debug(f'Copying PAM data from {pam_data_path_src} to {pam_data_path_dst}')
         shutil.copy(pam_data_path_src, pam_data_path_dst)
+        # shutil.copy(pam_data_path_src, os.path.join(self.ctx['dataRoot'], DATA_RELEASE)) # NOTE: this may be useful for L2_SM
         self.log.debug('Done.')
 
 
